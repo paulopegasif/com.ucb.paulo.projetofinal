efdad7ffa8f8fe1b4847b0d6a170056152dac18d
Paulo Henrique Pereira Silva <paulohenrique.psilva@a.ucb.br>
2025-06-14 19:53:11 -0300

    implementação do algoritmo de busca KMP

---
+++ src/main/java/com/ucb/paulo/projetofinal/util/KMP.java
@@ -0,0 +1,52 @@
+package com.ucb.paulo.projetofinal.util;
+
+public class KMP {
+
+    public static boolean containsPattern(String text, String pattern) {
+        int[] lps = computeLPSArray(pattern);
+        int i = 0; // índice para text
+        int j = 0; // índice para pattern
+
+        while (i < text.length()) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+
+            if (j == pattern.length()) {
+                return true; // padrão encontrado
+            } else if (i < text.length() && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return false; // padrão não encontrado
+    }
+
+    private static int[] computeLPSArray(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0;
+        int i = 1;
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
+}
