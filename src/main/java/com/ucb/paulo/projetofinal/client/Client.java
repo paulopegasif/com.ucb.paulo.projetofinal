0abdda386f63d8732673e53a9e7a5f4c15c1a118
Paulo Henrique Pereira Silva <paulohenrique.psilva@a.ucb.br>
2025-06-15 10:16:07 -0300

    salvando resultados em um txt

--- src/main/java/com/ucb/paulo/projetofinal/client/Client.java
+++ src/main/java/com/ucb/paulo/projetofinal/client/Client.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class Client {
     private static final String HOST = "localhost";
@@ -13,22 +15,31 @@ public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o termo de busca: ");
-        String termo = scanner.nextLine();
+
 
         try (Socket socket = new Socket(HOST, PORTA)) {
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter fileWriter = new PrintWriter(new FileWriter("resultados.txt"));
 
+            System.out.print("Digite o termo de busca: ");
+            String termo = scanner.nextLine();
             // envia o termo
             writer.println(termo);
 
+            int count = 0;
+
             // recebe os resultados
-            System.out.println("\nResultados encontrados:\n");
             String linha;
             while (!(linha = reader.readLine()).equals("FIM")) {
-                System.out.println("• " + linha);
+                fileWriter.println(linha);
+                count++;
             }
+
+            fileWriter.close();
+
+            System.out.println("\nTotal encontrados: "+ count);
+            System.out.println("Resultados salvos no arquivo resultados.txt ");
 
         } catch (Exception e) {
             e.printStackTrace();
